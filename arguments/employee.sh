eb19cc33316801f6725398a1415ed4cf06093dc6
Kartik <kartikgautam677@gmail.com>
2025-03-08 05:05:19 +0000

    Arguments in Shell Scripting

---
+++ arguments/employee.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "My name is $1 and I am working in $2 since $3 years"
+
+#We just need to give Name as first argument, Company name as second argument and years as third argument.
---
+++ arguments/greet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello, $1! Welcome to bash scripting using arguments"
+
+#Just Provide some name(argument) when we run the script like
+#./greet.sh john
+
+#Then it will print Hello, John! Welcome to bash scripting using arguments.
+
+
